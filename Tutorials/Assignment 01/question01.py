d3935dcce779a0f2d2e9442c30424c3916df5631
Ankita Das <adas22156@gmail.com>
2023-11-26 18:30:19 +0530

    Assignment 01

---
+++ Tutorials/Assignment 01/question01.py
@@ -0,0 +1,4 @@
+# Question no 1 :: Create a variable name with your name and print a greeting using that variable.
+
+name = "Ankita Das"
+print (" Hiii", name.split()[0], "! Good evening ..")
---
+++ Tutorials/Assignment 01/question04.py
@@ -0,0 +1,9 @@
+# Question no 4 ::Swap the values of two variables, a and b, and print the updated values.
+a= 2
+b= 3
+
+a=a+b
+b=a-b
+a=a-b 
+
+print ("Updated value of a and b is respectively :: ",a,b)
