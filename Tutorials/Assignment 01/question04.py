65df60e28f814465d8e65aedb650c31773efd38c
Ankita Das <adas22156@gmail.com>
2023-11-28 21:55:34 +0530

    Assignment 02

--- Tutorials/Assignment 01/question04.py
+++ Tutorials/Assignment 01/question04.py
@@ -7,3 +7,9 @@ b=a-b
 a=a-b 
 
 print ("Updated value of a and b is respectively :: ",a,b)
+
+# pythonic way 
+x=90
+y=99
+x, y = y, x
+print ("Updated value of x and y  is respectively :: ",x , y )
